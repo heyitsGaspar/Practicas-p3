447fa7292b8a75f184432674ddc7f0686d97f1c8
heyitsGaspar <israel.chay.32@gmail.com>
2024-07-20 13:45:57 -0600

    Practicas de codelabs parcial 3

---
+++ flutter_adding_admob/android/app/src/main/kotlin/com/example/flutter_adding_admob/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_adding_admob
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter_adding_admob/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:awesome_drawing_quiz/app_theme.dart';
+import 'package:awesome_drawing_quiz/game_route.dart';
+import 'package:awesome_drawing_quiz/home_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+  ));
+
+  runApp(
+    MaterialApp(
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => const HomeRoute(),
+        '/game': (BuildContext context) => const GameRoute()
+      },
+      theme: ThemeData(
+        primaryColor: AppTheme.primary,
+        primaryColorDark: AppTheme.primaryDark,
+        textTheme: GoogleFonts.acmeTextTheme().copyWith(
+            labelLarge: GoogleFonts.ubuntuMono(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        )),
+        buttonTheme: ButtonThemeData(
+          buttonColor: AppTheme.accent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24),
+          ),
+          textTheme: ButtonTextTheme.primary,
+        ),
+        colorScheme:
+            ColorScheme.fromSwatch().copyWith(secondary: AppTheme.accent),
+      ),
+    ),
+  );
+}
